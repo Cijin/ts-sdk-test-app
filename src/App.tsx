624691ef0710df1b7a1c43c781d33d9bc8e22b98
Seagin <cijininurhood@gmail.com>
2022-08-08 12:27:55 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {
+  createQuery,
+  runQuery,
+  createComment,
+  connectClient,
+  enableCache,
+  createClient,
+  subscribeTopic,
+  getPostTopic,
+  observePost,
+  observeComments,
+} from "@amityco/ts-sdk";
+
+import "./App.css";
+
+const referenceType = "post";
+const referenceId = "62e8f70ac4daed00d90209fb";
+const text = "example post";
+
+const apiKey = "b3bee858328ef4344a308e4a5a091688d05fdee2be353a2b";
+const apiRegion = "staging";
+const session = { userId: "test" };
+const path =
+  "5b028e4a673f81000fb040e7/social/user/5d2f0001369828e7a92bfab6/post/62e8f70ac4daed00d90209fb";
 
 function App() {
+  const [connected, setConnected] = React.useState(false);
+  const [comment, setComment] = React.useState({});
+  const [client, setClient] = React.useState({});
+
+  /*
+   * Connect Client
+   */
+  const connect = async () => {
+    setClient(createClient(apiKey, apiRegion));
+    // @ts-ignore
+    window.amityClient = client;
+
+    try {
+      await connectClient(session);
+      setConnected(true);
+      enableCache();
+      return { success: true };
+    } catch (e) {
+      setConnected(false);
+    }
+  };
+
+  // create new comment
+  const newComment = () => {
+    // @ts-ignore
+    const query = createQuery(createComment, {
+      referenceId,
+      referenceType,
+      data: { text },
+    });
+    runQuery(query, (data) => setComment(data));
+  };
+
+  // subscribe to post
+  const subscribe = () => {
+    observePost(referenceId, {
+      onFetch: subscribeTopic(
+        // @ts-ignore
+        getPostTopic({ path }, "comment")
+      ),
+      onEvent: (params) => console.log(params),
+    });
+  };
+
+  const subscribeComment = () => {
+    observeComments(referenceId, {
+      onEvent: console.log,
+    });
+  };
+
+  // @ts-ignore
+  const printCache = () => console.log(client.cache);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h3>Connected: {String(connected)}</h3>
+        <button onClick={connect}>Connect</button>
+        <hr />
+
+        <button onClick={newComment}>Create Comment</button>
+        <pre>{JSON.stringify(comment, null, 2)}</pre>
+      </section>
+      <hr />
+
+      <section>
+        <button onClick={subscribe}>Subscibe to Post-Comment</button>
+        <button onClick={printCache}>Print Cache</button>
+      </section>
     </div>
   );
 }
